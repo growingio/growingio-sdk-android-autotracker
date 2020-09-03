0064c6ee6c2604556e482dbbd4b8d67d43d0d678
styluo <shu_jiahuili@foxmail.com>
2020-09-03 22:15:11 +0800

    build: demo区分release

--- publishAutotrackerPluginToMavenLocal.sh
+++ publishAutotrackerPluginToMavenLocal.sh
@@ -5,5 +5,6 @@ export IS_EXCLUDE_DEMOS=true
 ./gradlew clean \
 && ./gradlew :growingio-autotracker-gradle-plugin:publishOfficialPublicationToMavenLocal \
 && ./gradlew :growingio-tracker:publishOfficialPublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker:publishOfficialPublicationToMavenLocal \
 && ./gradlew clean \
 && export IS_EXCLUDE_DEMOS=false
