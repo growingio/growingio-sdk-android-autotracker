1c6f8df552919cc47f80410a2e2701844ccbb08f
styluo <shu_jiahuili@foxmail.com>
2020-09-11 11:24:32 +0800

    perf: page按照新的逻辑修改

--- growingio-tracker/src/main/java/com/growingio/android/sdk/track/webservices/log/WsLogger.java
+++ growingio-tracker/src/main/java/com/growingio/android/sdk/track/webservices/log/WsLogger.java
@@ -22,58 +22,71 @@ import android.util.Log;
 
 import com.growingio.android.sdk.track.log.BaseLogger;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class WsLogger extends BaseLogger {
     public static final String TYPE = "WsLogger";
 
     private final CircularFifoQueue<LoggerDataMessage.LogItem> mCacheLogs = new CircularFifoQueue<>(100);
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
     private Callback mCallback;
 
     public void setCallback(Callback callback) {
-        mCallback = callback;
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                mCallback = callback;
+            }
+        });
     }
 
     @Override
     protected void print(int priority, @NonNull String tag, @NonNull String message, @Nullable Throwable t) {
-        String state;
-        switch (priority) {
-            case Log.VERBOSE:
-                state = "VERBOSE";
-                break;
-            case Log.DEBUG:
-                state = "DEBUG";
-                break;
-            case Log.INFO:
-                state = "INFO";
-                break;
-            case Log.WARN:
-                state = "WARN";
-                break;
-            case Log.ERROR:
-                state = "ERROR";
-                break;
-            case Log.ASSERT:
-                state = "ALARM";
-                break;
-            default:
-                state = "OTHER";
-        }
-
-        if (mCallback != null) {
-            if (!mCacheLogs.isEmpty()) {
-                mCallback.disposeLog(LoggerDataMessage.createMessage(mCacheLogs));
-                mCacheLogs.clear();
-            }
-            mCallback.disposeLog(LoggerDataMessage.
-                    createMessage(state,
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                String state;
+                switch (priority) {
+                    case Log.VERBOSE:
+                        state = "VERBOSE";
+                        break;
+                    case Log.DEBUG:
+                        state = "DEBUG";
+                        break;
+                    case Log.INFO:
+                        state = "INFO";
+                        break;
+                    case Log.WARN:
+                        state = "WARN";
+                        break;
+                    case Log.ERROR:
+                        state = "ERROR";
+                        break;
+                    case Log.ASSERT:
+                        state = "ALARM";
+                        break;
+                    default:
+                        state = "OTHER";
+                }
+                if (mCallback != null) {
+                    if (!mCacheLogs.isEmpty()) {
+                        mCallback.disposeLog(LoggerDataMessage.createMessage(mCacheLogs));
+                        mCacheLogs.clear();
+                    }
+                    mCallback.disposeLog(LoggerDataMessage.
+                            createMessage(state,
+                                    "xxx",
+                                    message,
+                                    String.valueOf(System.currentTimeMillis())));
+                } else {
+                    mCacheLogs.add(LoggerDataMessage.createLogItem(state,
                             "xxx",
                             message,
                             String.valueOf(System.currentTimeMillis())));
-        } else {
-            mCacheLogs.add(LoggerDataMessage.createLogItem(state,
-                    "xxx",
-                    message,
-                    String.valueOf(System.currentTimeMillis())));
-        }
+                }
+            }
+        });
     }
 
     @Override
