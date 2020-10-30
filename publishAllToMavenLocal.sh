9b44a9705c5534054d653018b1882d62901dbf05
styluo <shu_jiahuili@foxmail.com>
2020-10-30 14:06:06 +0800

    feat: 拆分包

--- publishAllToMavenLocal.sh
+++ publishAllToMavenLocal.sh
@@ -2,10 +2,10 @@
 
 export IS_EXCLUDE_DEMOS=true
 ./gradlew clean \
-&& ./gradlew :growingio-autotracker-gradle-plugin:publishOfficialPublicationToMavenLocal \
-&& ./gradlew :growingio-tracker:publishOfficialPublicationToMavenLocal \
-&& ./gradlew :growingio-tracker-api:publishOfficialPublicationToMavenLocal \
-&& ./gradlew :growingio-autotracker:publishOfficialPublicationToMavenLocal \
-&& ./gradlew :growingio-autotracker-api:publishOfficialPublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker-gradle-plugin:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-tracker:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-tracker-api:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker-api:publishReleasePublicationToMavenLocal \
 && ./gradlew clean \
 && export IS_EXCLUDE_DEMOS=false
