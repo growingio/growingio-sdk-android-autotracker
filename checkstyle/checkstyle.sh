dcc7d23554ce3695b918606b3e08706342674945
styluo <shu_jiahuili@foxmail.com>
2020-10-30 15:57:35 +0800

    feat: 打包脚本增加javadoc及sources

--- checkstyle/checkstyle.sh
+++ checkstyle/checkstyle.sh
@@ -1,5 +1,7 @@
-./gradlew :growingio-tracker:checkstyle \
-&& ./gradlew :growingio-autotracker:checkstyle \
+./gradlew :growingio-tracker-api:checkstyle \
+&& ./gradlew :growingio-tracker-core:checkstyle \
+&& ./gradlew :growingio-autotracker-api:checkstyle \
+&& ./gradlew :growingio-autotracker-core:checkstyle \
 && ./gradlew :growingio-autotracker-gradle-plugin:checkstyle \
 && ./gradlew :inject-compiler:checkstyle \
 && ./gradlew :inject-annotation:checkstyle \
