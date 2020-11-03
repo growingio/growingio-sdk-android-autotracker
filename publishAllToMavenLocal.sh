c438b05ae6d5634817049583b0556bea4bad6e20
xiangyang <xiangyang@growingio.com>
2020-11-03 11:44:37 +0800

    perf: module和artifact id保持同名

--- publishAllToMavenLocal.sh
+++ publishAllToMavenLocal.sh
@@ -4,8 +4,8 @@ export IS_EXCLUDE_DEMOS=true
 ./gradlew clean \
 && ./gradlew :growingio-autotracker-gradle-plugin:publishToMavenLocal \
 && ./gradlew :growingio-tracker-core:publishReleasePublicationToMavenLocal \
-&& ./gradlew :growingio-tracker-api:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-tracker:publishReleasePublicationToMavenLocal \
 && ./gradlew :growingio-autotracker-core:publishReleasePublicationToMavenLocal \
-&& ./gradlew :growingio-autotracker-api:publishReleasePublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker:publishReleasePublicationToMavenLocal \
 && ./gradlew clean \
 && export IS_EXCLUDE_DEMOS=false
