1d73f918608edb08b21a45f7315fd7f144734a4d
shenliming <348515494@qq.com>
2021-04-12 15:07:22 +0800

    v3.0.1 test publish

--- gradle/publishAllToMavenLocal.sh
+++ gradle/publishAllToMavenLocal.sh
@@ -2,10 +2,10 @@
 
 export IS_EXCLUDE_DEMOS=true
 ./gradlew clean \
-&& ./gradlew :growingio-autotracker-gradle-plugin:publishToMavenLocal \
-&& ./gradlew :growingio-tracker-core:publishToMavenLocal \
-&& ./gradlew :growingio-tracker:publishToMavenLocal \
-&& ./gradlew :growingio-autotracker-core:publishToMavenLocal \
-&& ./gradlew :growingio-autotracker:publishToMavenLocal \
+&& ./gradlew :growingio-autotracker-gradle-plugin:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-tracker-core:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-tracker:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker-core:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew clean \
 && export IS_EXCLUDE_DEMOS=false
