dbba44bbde6cd5206bacfeab98d1ff7216be6549
rq <yanruixue@growingio.com>
2021-07-08 14:37:54 +0800

    fix: 增加过滤事件日志

--- growingio-tracker-core/src/main/java/com/growingio/android/sdk/track/TrackMainThread.java
+++ growingio-tracker-core/src/main/java/com/growingio/android/sdk/track/TrackMainThread.java
@@ -106,6 +106,7 @@ public final class TrackMainThread extends ListenerContainer<OnTrackMainInitSDKC
         // 判断当前事件类型是否被过滤
         int filterMask = ConfigurationProvider.core().getEventFilterMask();
         if (filterMask != 0 && ((filterMask & FilterEventParams.valueOf(eventBuilder.getEventType())) > 0)) {
+            Logger.d(TAG, eventBuilder.getEventType() + " event is filtered ..." );
             return;
         }
 
