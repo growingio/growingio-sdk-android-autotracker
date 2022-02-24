329d577d00c1a5152b6c7f456d4a3e9dc4fa28fb
YoloMao <a83506677@126.com>
2022-02-24 15:16:00 +0800

    fix: cdp trackCustomEvent add eventName check

--- gio-sdk/tracker-cdp/src/main/java/com/growingio/android/sdk/track/CdpTracker.java
+++ gio-sdk/tracker-cdp/src/main/java/com/growingio/android/sdk/track/CdpTracker.java
@@ -54,8 +54,8 @@ public class CdpTracker extends Tracker {
 
     public void trackCustomEvent(String eventName, Map<String, String> attributes, String itemKey, String itemId) {
         if (!isInited) return;
-        if (TextUtils.isEmpty(itemKey) || TextUtils.isEmpty(itemId)) {
-            Logger.e(TAG, "trackCustomEvent: itemKey or itemId is NULL");
+        if (TextUtils.isEmpty(eventName) || TextUtils.isEmpty(itemKey) || TextUtils.isEmpty(itemId)) {
+            Logger.e(TAG, "trackCustomEvent: eventName, itemKey or itemId is NULL");
             return;
         }
 
