90758f0b85081fec0725988f80fb6c74a8d27c87
styluo <shu_jiahuili@foxmail.com>
2022-06-01 13:12:53 +0800

    feat: 适配GA3

--- gradle/publishAllToMavenLocal.sh
+++ gradle/publishAllToMavenLocal.sh
@@ -19,6 +19,7 @@ export IS_EXCLUDE_DEMOS=true
 && ./gradlew :growingio-tools:crash:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew :growingio-tools:oaid:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew :growingio-adapter:analytics:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-adapter:analytics3:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew :gio-sdk:tracker:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew :gio-sdk:tracker-cdp:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew :gio-sdk:autotracker:publishMavenAgentPublicationToMavenLocal \
