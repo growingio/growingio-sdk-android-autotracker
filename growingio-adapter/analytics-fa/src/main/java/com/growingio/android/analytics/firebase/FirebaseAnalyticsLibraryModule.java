0b91db40bcbf394d922f8928fb6c12e0a56a69f5
shenliming <348515494@qq.com>
2022-07-01 15:56:14 +0800

    checkstyle

--- growingio-adapter/analytics-fa/src/main/java/com/growingio/android/analytics/firebase/FirebaseAnalyticsLibraryModule.java
+++ growingio-adapter/analytics-fa/src/main/java/com/growingio/android/analytics/firebase/FirebaseAnalyticsLibraryModule.java
@@ -17,10 +17,8 @@ package com.growingio.android.analytics.firebase;
 
 import android.content.Context;
 
-import com.growingio.android.sdk.CoreConfiguration;
 import com.growingio.android.sdk.LibraryGioModule;
 import com.growingio.android.sdk.track.modelloader.TrackerRegistry;
-import com.growingio.android.sdk.track.providers.ConfigurationProvider;
 import com.growingio.sdk.annotation.GIOLibraryModule;
 
 /**
