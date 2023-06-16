1458463ec0eef04fd27435b3d9ff7cbc84a1034c
shenliming <348515494@qq.com>
2023-06-16 10:02:31 +0800

    checkstyle

--- growingio-autotracker-core/src/main/java/com/growingio/android/sdk/autotrack/view/ViewNode.java
+++ growingio-autotracker-core/src/main/java/com/growingio/android/sdk/autotrack/view/ViewNode.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
