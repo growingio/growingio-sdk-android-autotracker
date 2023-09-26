b53ea13da23800ae7e9d360d4469a5dec35e95ee
shenliming <348515494@qq.com>
2023-09-26 15:57:51 +0800

    spotlessCheck

--- gio-sdk/autotracker-saas/src/main/java/com/growingio/android/sdk/autotrack/UpgradeProvider.java
+++ gio-sdk/autotracker-saas/src/main/java/com/growingio/android/sdk/autotrack/UpgradeProvider.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.growingio.android.sdk.autotrack;
 
 import android.content.Context;
