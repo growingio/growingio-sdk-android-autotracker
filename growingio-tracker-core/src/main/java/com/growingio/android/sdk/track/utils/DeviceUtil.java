44290ba799c1009796e9c3a06ff2356f8bd0abdc
shenliming <348515494@qq.com>
2023-12-06 11:36:38 +0800

    Fix IncorrectContextUseViolation on Android 11. When StrictMode is enabled, Android 11 will display a warning if the app requests `WindowManager` on application contexts.

--- growingio-tracker-core/src/main/java/com/growingio/android/sdk/track/utils/DeviceUtil.java
+++ growingio-tracker-core/src/main/java/com/growingio/android/sdk/track/utils/DeviceUtil.java
@@ -15,22 +15,27 @@
  */
 package com.growingio.android.sdk.track.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.hardware.display.DisplayManager;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.Display;
-import android.view.WindowManager;
 
 public class DeviceUtil {
     private DeviceUtil() {
     }
 
+    @SuppressLint("WrongConstant")
     public static DisplayMetrics getDisplayMetrics(Context context) {
         DisplayMetrics metrics = new DisplayMetrics();
-        Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        if (display == null) return context.getResources().getDisplayMetrics();
-        display.getRealMetrics(metrics);
-        return metrics;
+        DisplayManager displayManager = ((DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE));
+        if (displayManager != null) {
+            Display display = displayManager.getDisplay(Display.DEFAULT_DISPLAY);
+            if (display != null) display.getRealMetrics(metrics);
+            return metrics;
+        }
+        return context.getResources().getDisplayMetrics();
     }
 
     public static boolean isPhone(Context context) {
