7b3b9af11dafd3e32e48c903f7352d67d01590c6
cpacm <shenliming1013@gmail.com>
2024-03-27 16:09:58 +0800

    feat: event data size limit

--- growingio-data/database/src/main/java/com/growingio/android/database/EventDataManager.java
+++ growingio-data/database/src/main/java/com/growingio/android/database/EventDataManager.java
@@ -46,6 +46,7 @@ public class EventDataManager {
     private static final String TAG = "EventDataManager";
 
     private static final long EVENT_VALID_PERIOD_MILLS = 7L * 24 * 60 * 60_000;
+    private static final double EVENT_DATA_MAX_SIZE = 2 * 1024 * 1024;
 
     private final TrackerContext context;
     private final String eventsInfoAuthority;
@@ -131,23 +132,27 @@ public class EventDataManager {
             dbResult.setSuccess(false);
             return;
         }
-        long lastId = -1;
         List<byte[]> queryList = new ArrayList<>();
         ContentProviderClient client = context.getContentResolver().acquireContentProviderClient(eventsInfoAuthority);
         try (Cursor cursor = queryEvents(client, policy, limit)) {
             int count = 0;
+            double dataSize = 0;
             while (cursor.moveToNext()) {
                 count++;
                 String eventType = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_EVENT_TYPE));
                 dbResult.setEventType(eventType);
-                if (cursor.isLast()) {
-                    lastId = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
-                    dbResult.setLastId(lastId);
-                }
+                long lastId = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
+                dbResult.setLastId(lastId);
                 byte[] data = cursor.getBlob(cursor.getColumnIndexOrThrow(COLUMN_DATA));
-                if (data != null) {
+                if (data != null && data.length < EVENT_DATA_MAX_SIZE) {
+                    dataSize += data.length;
+                    if (dataSize > EVENT_DATA_MAX_SIZE) {
+                        break;
+                    }
                     queryList.add(data);
                 } else {
+                    Logger.e(TAG, "event data is too large or null, delete it.");
+                    // event data is illegal, delete it.
                     long delId = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
                     removeEventById(client, delId);
                 }
@@ -182,13 +187,18 @@ public class EventDataManager {
         List<byte[]> queryList = new ArrayList<>();
         try (Cursor cursor = queryEvents(client, policy, limit)) {
             int count = 0;
+            double dataSize = 0;
             while (cursor.moveToNext()) {
                 count++;
-                if (cursor.isLast()) {
-                    cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
-                }
+                long lastId = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
+                dbResult.setLastId(lastId);
+
                 byte[] data = cursor.getBlob(cursor.getColumnIndexOrThrow(COLUMN_DATA));
-                if (data != null) {
+                if (data != null && data.length < EVENT_DATA_MAX_SIZE) {
+                    dataSize += data.length;
+                    if (dataSize > EVENT_DATA_MAX_SIZE) {
+                        break;
+                    }
                     queryList.add(data);
                 }
                 long delId = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_ID));
