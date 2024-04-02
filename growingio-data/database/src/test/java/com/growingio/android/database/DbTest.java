1f91e4bf67f5192ca24450c7e61453e4ac0813c1
cpacm <shenliming1013@gmail.com>
2024-04-02 11:25:49 +0800

    add db large data test.

--- growingio-data/database/src/test/java/com/growingio/android/database/DbTest.java
+++ growingio-data/database/src/test/java/com/growingio/android/database/DbTest.java
@@ -17,6 +17,7 @@ package com.growingio.android.database;
 
 import android.app.Application;
 import android.content.pm.ProviderInfo;
+import android.net.Uri;
 
 import androidx.test.core.app.ApplicationProvider;
 
@@ -24,6 +25,7 @@ import com.google.common.truth.Truth;
 import com.growingio.android.json.JsonDataLoader;
 import com.growingio.android.sdk.Tracker;
 import com.growingio.android.sdk.TrackerContext;
+import com.growingio.android.sdk.track.events.AttributesBuilder;
 import com.growingio.android.sdk.track.events.CustomEvent;
 import com.growingio.android.sdk.track.events.PageEvent;
 import com.growingio.android.sdk.track.middleware.EventDatabase;
@@ -207,6 +209,33 @@ public class DbTest {
         dataManager.queryEvents(customEvent.getSendPolicy(), 200, eventDbResult);
         assertThat(eventDbResult.getSum()).isEqualTo(150);
         assertThat(eventDbResult.isSuccess()).isEqualTo(true);
+    }
+
+    @Test
+    public void insetLargeData() {
+        trackerContext.getRegistry().register(EventFormatData.class, EventByteArray.class, new ProtobufDataLoader.Factory());
+        controller.create(providerInfo).get();
+        long dataSize = 0;
+        long maxDataSize = 2 * 1000 * 1024;
+        int key = 1;
+        AttributesBuilder attrBuilder = new AttributesBuilder();
+        String data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in scelerisque sem. Mauris volutpat, dolor id interdum ullamcorper, risus dolor egestas lectus, sit amet mattis purus dui nec risus. Maecenas non sodales nisi, vel dictum dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse blandit eleifend diam, vel rutrum tellus vulputate quis. Aliquam eget libero aliquet, imperdiet nisl a, ornare ex. Sed rhoncus est ut libero porta lobortis. Fusce in dictum tellus.\n" +
+                "    \\n\\n\\t\\tSuspendisse interdum ornare ante. Aliquam nec cursus lorem. Morbi id magna felis. Vivamus egestas, est a condimentum egestas, turpis nisl iaculis ipsum, in dictum tellus dolor sed neque. Morbi tellus erat, dapibus ut sem a, iaculis tincidunt dui. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur et eros porttitor, ultricies urna vitae, molestie nibh. Phasellus at commodo eros, non aliquet metus. Sed maximus nisl nec dolor bibendum, vel congue leo egestas.\n" +
+                "    \\n\\n\\t\\tSed interdum tortor nibh, in sagittis risus mollis quis. Curabitur mi odio, condimentum sit amet auctor at, mollis non turpis. Nullam pretium libero vestibulum, finibus orci vel, molestie quam. Fusce blandit tincidunt nulla, quis sollicitudin libero facilisis et. Integer interdum nunc ligula, et fermentum metus hendrerit id. Vestibulum lectus felis, dictum at lacinia sit amet, tristique id quam. Cras eu consequat dui. Suspendisse sodales nunc ligula, in lobortis sem porta sed. Integer id ultrices magna, in luctus elit. Sed a pellentesque est.\n" +
+                "    \\n\\n\\t\\tAenean nunc velit, lacinia sed dolor sed, ultrices viverra nulla. Etiam a venenatis nibh. Morbi laoreet, tortor sed facilisis varius, nibh orci rhoncus nulla, id elementum leo dui non lorem. Nam mollis ipsum quis auctor varius. Quisque elementum eu libero sed commodo. In eros nisl, imperdiet vel imperdiet et, scelerisque a mauris. Pellentesque varius ex nunc, quis imperdiet eros placerat ac. Duis finibus orci et est auctor tincidunt. Sed non viverra ipsum. Nunc quis augue egestas, cursus lorem at, molestie sem. Morbi a consectetur ipsum, a placerat diam. Etiam vulputate dignissim convallis. Integer faucibus mauris sit amet finibus convallis.\n" +
+                "    \\n\\n\\t\\tPhasellus in aliquet mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In volutpat arcu ut felis sagittis, in finibus massa gravida. Pellentesque id tellus orci. Integer dictum, lorem sed efficitur ullamcorper, libero justo consectetur ipsum, in mollis nisl ex sed nisl. Donec maximus ullamcorper sodales. Praesent bibendum rhoncus tellus nec feugiat. In a ornare nulla. Donec rhoncus libero vel nunc consequat, quis tincidunt nisl eleifend. Cras bibendum enim a justo luctus vestibulum. Fusce dictum libero quis erat maximus, vitae volutpat diam dignissim.\n" +
+                "  ";
+        while (dataSize < maxDataSize) {
+            attrBuilder.addAttribute(String.valueOf(key), data);
+            key++;
+            dataSize += data.getBytes().length + String.valueOf(key).getBytes().length;
+        }
+        CustomEvent customEvent = new CustomEvent.Builder()
+                .setEventName("LargeData")
+                .setAttributes(attrBuilder.build())
+                .build();
+        Uri result = sqLite.insertEvent(customEvent);
+        assertThat(result).isNull();
     }
 
 
